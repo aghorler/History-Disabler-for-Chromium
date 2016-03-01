566cbd16436e85b91115fa1059c96fe1ab8087ab
rentamob <aghorler@gmail.com>
2016-03-01 17:39:50 +1100

    Incorrect directory
    Moved background.js to /js
    Removed old /js/background.js

--- js/background.js
+++ js/background.js
@@ -1,5 +1,30 @@
+/*	Function to clear all browsing history using the BrowsingData API. */
+function clearBrowsingData() {
+	chrome.browsingData.removeHistory({
+		"originTypes": {
+			"protectedWeb": true,
+			"unprotectedWeb": true,
+			"extension": true
+		}
+	});
+}
+
+/*	Clear all browsing history on install. */
+chrome.runtime.onInstalled.addListener(function(details){
+	if(details.reason == "install"){
+		clearBrowsingData();
+	}
+});
+
+/*	Remove a history item immediately after being generated. */
 chrome.history.onVisited.addListener(function(historyItem) {
 	chrome.history.deleteUrl({
 		"url": historyItem.url
 	});
+});
+
+/*	Clear history on tab close. 
+	This clears browsing history listed in 'Recently closed'. */
+chrome.tabs.onRemoved.addListener(function() {
+	clearBrowsingData();
 });
