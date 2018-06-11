fbfd054641cdcb9ec8162e16dc48ae0a34402008
Aaron Horler <git@aaronhorler.com>
2018-06-11 18:37:39 +1000

    Remove "retain recently closed tabs option".
    This is no longer relevant it seems.

--- js/options.js
+++ js/options.js
@@ -1,9 +1,7 @@
 function saveOptions(){
   var downloadOption = document.getElementById('download_option').checked;
-  var tabsOption = document.getElementById('tabs_option').checked;
   chrome.storage.local.set({
-    clearDownloads: downloadOption,
-    retainTabs: tabsOption
+    clearDownloads: downloadOption
   });
 }
 
@@ -13,10 +11,8 @@ function restoreOptions(){
     retainTabs: false
   }, function(items){
     document.getElementById('download_option').checked = items.clearDownloads;
-    document.getElementById('tabs_option').checked = items.retainTabs;
   });
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.getElementById('download_option').addEventListener('change', saveOptions);
-document.getElementById('tabs_option').addEventListener('change', saveOptions);
